6f63223d6e3ec7e217132aa71dbe6c7529de3a09
AnhNguyen-Olivia <thuyanhnguyen.vietnam@gmail.com>
2026-04-14 11:54:03 +0700

    Refactor the program to make it more suitable for UI and CLI, clean the main and the helper no longer will be use but will be remember :')

---
+++ src/main/java/ftpclient/com/FtpUI.java
@@ -0,0 +1,12 @@
+package ftpclient.com;
+
+
+public class FtpUI {
+
+    public void startUI() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'startUI'");
+    }
+    
+
+}
